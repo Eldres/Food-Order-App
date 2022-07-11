3c29068ece025bfc6749f5c1700a7aab2d00f82f
Josh Nagel <nageljp27@gmail.com>
2022-07-11 14:55:56 -0600

    added a context and provider component.

---
+++ src/store/CartProvider.js
@@ -0,0 +1,21 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const handleAddItemToCart = (item) => {};
+  const handleRemoveItemFromCart = (id) => {};
+
+  const cartContext = {
+    item: [],
+    totalAmount: 0,
+    addItem: handleAddItemToCart,
+    removeItem: handleRemoveItemFromCart,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
