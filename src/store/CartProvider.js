4ccb722f822fec88ea0e92ba1a03e577a0da3312
Josh Nagel <nageljp27@gmail.com>
2022-07-11 15:20:37 -0600

    added a Cart reducer to manage cart state for adding and removing items

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,42 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (cartState, action) => {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = cartState.items.concat(action.payload);
+    const updatedTotalAmount =
+      cartState.totalAmount + action.payload.price * action.payload.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const handleAddItemToCart = (item) => {};
-  const handleRemoveItemFromCart = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const handleAddItemToCart = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", payload: item });
+  };
+  const handleRemoveItemFromCart = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", payload: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: handleAddItemToCart,
     removeItem: handleRemoveItemFromCart,
   };
