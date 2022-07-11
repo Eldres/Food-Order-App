a1195c568ba0c65159c64d283e3119c24600487d
Josh Nagel <nageljp27@gmail.com>
2022-07-11 15:54:22 -0600

    implemented logic for adding item to cart using useState

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Input from "../../UI/Input/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [enteredAmount, setEnteredAmount] = useState(1);
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const handleChange = (event) => {
+    setEnteredAmount(Number(event.target.value));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmount);
+    setEnteredAmount(1);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <Input
         label="Amount"
         input={{
@@ -13,9 +32,12 @@ const MealItemForm = (props) => {
           max: "5",
           step: "1",
           defaultValue: "1",
+          value: enteredAmount,
+          onChange: handleChange,
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
