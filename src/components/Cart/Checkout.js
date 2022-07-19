4e1b9635bfb14e1f15190a230e4729d80d065f27
Josh Nagel <nageljp27@gmail.com>
2022-07-19 15:18:52 -0600

    added reading form data

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,32 +1,45 @@
+import { useRef } from "react";
 import classes from "./Checkout.module.css";
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const handleConfirm = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
-    <form onSubmit={handleConfirm}>
+    <form className={classes.form} onSubmit={handleConfirm}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name:</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="name">Street</label>
-        <input type="text" id="street" />
+        <input type="text" id="street" ref={streetInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="name">Postal Code</label>
-        <input type="text" id="postal" />
+        <input type="text" id="postal" ref={postalInputRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="name">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" ref={cityInputRef} />
       </div>
-      <button>Confirm</button>
-      <button type="button" onClick={props.onCancel}>
-        Cancel
-      </button>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
